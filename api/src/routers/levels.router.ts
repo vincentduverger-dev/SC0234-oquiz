202cdd1dd03ff3852cd760392537f94a608f771a
Charly_P <parisatocharly@gmail.com>
2026-02-03 14:10:52 +0100

    SC03E02 - (api) feat : checkRoles sur GET levels

--- api/src/routers/levels.router.ts
+++ api/src/routers/levels.router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as levelsController from "../controllers/levels.controller.ts";
+import { checkRoles } from "../middlewares/accessControl.middleware.ts";
 
 export const router = Router();
 
 // Récupérer tous les levels
-router.get("/levels", levelsController.getAllLevels);
+router.get("/levels", checkRoles(["member", "author", "admin"]), levelsController.getAllLevels);
 
 // Récupérer un level par son identifiant
 router.get("/levels/:id", levelsController.getLevelById);
